926630848ccf30ed1e8a3ad38297717d256494d9
Slothfulsilver <arturo_garciagb@hotmail.com>
2023-10-21 04:41:48 -0600

    Done/Search Broken

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ app.engine('html',require('ejs').renderFile)
 app.set("view Engine", "html");
 app.use(exp.static(__dirname + '/public'));
 
+const url = "https://thronesapi.com/api/v2/Characters";
+
+
+
 app.get("/", (req, res) => {
-    const url = "https://thronesapi.com/api/v2/Characters";
-
-
+    
     https.get(url, (response) => {
         let respData = '';
         
@@ -18,13 +20,23 @@ app.get("/", (req, res) => {
             })   
             .on('end', (data) => {
                 const gotData = JSON.parse(respData);
-                console.log(gotData);
                 res.render('index.ejs', {data: gotData});
             })
     });
     
    
 });
+
+app.get("/error", (req, res) => {
+    app.render("error.ejs")
+})
+
+app.get("*", (req, res) => { 
+  
+    // Here user can also design an 
+    // error page and render it  
+    res.render('error.ejs');
+  }); 
 
 app.listen(3000, () => {
 
